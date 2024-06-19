95de222cc2000d36d08712012e7903e8cb6a997c
Alfredocaconda <123181679+Alfredocaconda@users.noreply.github.com>
2024-06-19 19:13:46 -0300

    fixed

--- app/Http/Controllers/Admin/SuportController.php
+++ app/Http/Controllers/Admin/SuportController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Suport;
 use Illuminate\Http\Request;
 
 class SuportController extends Controller
 {
     public function index(){
-        return view('Admin/Suport/index');
+        $suports= Suport::all();
+        return view('Admin/Suport/index',compact('suports'));
     }
 }
--- resources/views/Admin/Suport/index.blade.php
+++ resources/views/Admin/Suport/index.blade.php
@@ -1,1 +1,22 @@
 <h2>Listagens dos suportes</h2>
+
+<table>
+    <thead>
+        <th>Objectivo</th>
+        <th>status</th>
+        <th>informacoes</th>
+        <th></th>
+    </thead>
+    <tbody>
+        @foreach ($suports as $suport)
+            <tr>
+                <td>{{$suport->objectivo}}</td>
+                <td>{{$suport->status}}</td>
+                <td>{{$suport->informacoes}}</td>
+                <td>
+                    >
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
