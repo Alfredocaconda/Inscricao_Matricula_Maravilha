caec75a8dbbbf09346913c37d2262b2994a7063b
Alfredocaconda <123181679+Alfredocaconda@users.noreply.github.com>
2024-06-29 20:16:01 -0300

    valores

--- resources/views/Admin/Suport/create.blade.php
+++ resources/views/Admin/Suport/create.blade.php
@@ -1,10 +1,14 @@
 <h1> NOVO SUPORTE</h1>
 {{--- CODIGO PARA PODER EXIBIR MENSAGEM--}}
+{{--obtemos uma condicao para saber se existe erro--}}
 @if ($errors->any())
+{{--inserimos um ciclo de repeticao para poder comparar varios valores--}}
     @foreach ($errors->all() as $error)
+    {{--script para apresentar informacoes na tela--}}
        <script>alert('{{$error}}');</script>
     @endforeach
 @endif
+{{--fim da condicao--}}
 <form action="{{route('suport.store')}}" method="post">
     <!---- input para pegar o token----->
     {{--<input type="hidden" value="{{csrf_token()}}" name="_token">---}}
